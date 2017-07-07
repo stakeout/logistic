d566ba8e892cc488096c48d4772f1fd8f56e6228
Vadim Novash <novash@tut.by>
2017-07-07 15:44:51 +0300

    solutions markup, contacts page added

---
+++ markup/components/solutions/data/data.js
@@ -0,0 +1,56 @@
+solutions: {
+    defaults: {
+        title: 'Мидав',
+        useSocialMetaTags: false
+    },
+    elements: [
+        {
+          title: 'Автомобильная промышленность',
+          imgName: 'solutions-1'
+        },
+        {
+          title: 'Алкогольная промышленность',
+          imgName: 'solutions-2'
+        },
+        {
+          title: 'Деревообрабатывающая промышленность',
+          imgName: 'solutions-3'
+        },
+        {
+          title: 'Индустрия моды',
+          imgName: 'solutions-4'
+        },
+        {
+          title: 'Машиностроительная промышленность',
+          imgName: 'solutions-5'
+        },
+        {
+          title: 'Продукты питания',
+          imgName: 'solutions-6'
+        },
+        {
+          title: 'Сельхозтехника и оборудование',
+          imgName: 'solutions-7'
+        },
+        {
+          title: 'Техника и электроника',
+          imgName: 'solutions-8'
+        },
+        {
+          title: 'Товары народного потребления',
+          imgName: 'solutions-9'
+        },
+        {
+          title: 'Фармацевтическая отрасль',
+          imgName: 'solutions-10'
+        },
+        {
+          title: 'Химическая отрасль',
+          imgName: 'solutions-11'
+        },
+        {
+          title: 'Целлюлозно-бумажная отрасль',
+          imgName: 'solutions-12'
+        }
+    ]
+}
