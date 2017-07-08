af242dccf82f6b6f730522d605c306cd8324d895
Vadim Novash <novash@tut.by>
2017-07-08 17:15:05 +0300

    clients block done without js, slick carousel added

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,4 +1,9 @@
-// import '../../components/navigation/navigation';
+import '../../components/slick/slick.min';
 $(() => {
-
+    $('.clients__list').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        focusOnSelect: true
+    });
 });
