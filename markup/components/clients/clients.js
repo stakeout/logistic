2eb02b486c49b1a3e623a8d3f7bd2ea6513463d1
Vadim Novash <novash@tut.by>
2017-07-10 17:58:22 +0300

    js filter on clients done

--- markup/components/clients/clients.js
+++ markup/components/clients/clients.js
@@ -1,26 +1,26 @@
 export const filterByType = () => {
     const elem = $('.filter__item');
     const carousel = $('.clients__list');
-    function addActiveClass(e) {
-        console.log(this);
+    const carouselItem = $('.clients__item');
+    function addActiveClass() {
         for (let i = 0; i < elem.length; i++) {
             elem[i].classList.remove('active');
         }
         this.classList.add('active');
+        let btnType = this.getAttribute('data-type');
         let filtered = false;
-        if (filtered === false) {
-            carousel.slick('slickFilter', ':even');
-            filtered = true;
+        if (btnType !== 'all') {
+            carousel.slick('slickFilter', function (index) {
+                return ($(this).attr('data-type') === btnType);
+            });
+
         } else {
             carousel.slick('slickUnfilter');
-            filtered = false;
         }
     }
     if (elem) {
         for (let i = 0; i < elem.length; i++) {
             elem[i].addEventListener('click', addActiveClass);
         }
-        console.log('true');
-        // let elem = document.querySelectorAll('.filter__item');
     }
 };
