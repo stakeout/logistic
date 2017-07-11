3c7c9afcbec42279df62ee28eeeeaf2726ac181d
Vadim Novash <novash@tut.by>
2017-07-11 16:41:05 +0300

    request-form done

--- markup/components/clients/clients.js
+++ markup/components/clients/clients.js
@@ -1,16 +1,14 @@
 export const filterByType = () => {
     const elem = $('.filter__item');
     const carousel = $('.clients__list');
-    const carouselItem = $('.clients__item');
     function addActiveClass() {
+        let btnType = this.getAttribute('data-type');
         for (let i = 0; i < elem.length; i++) {
             elem[i].classList.remove('active');
         }
         this.classList.add('active');
-        let btnType = this.getAttribute('data-type');
-        let filtered = false;
         if (btnType !== 'all') {
-            carousel.slick('slickFilter', function (index) {
+            carousel.slick('slickFilter', function () {
                 return ($(this).attr('data-type') === btnType);
             });
 
