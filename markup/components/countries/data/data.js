73d976bd1f3098a1a543d1f5ab78ed416f538469
Vadim Novash <novash@tut.by>
2017-07-17 16:51:41 +0300

    european direction markup done

---
+++ markup/components/countries/data/data.js
@@ -0,0 +1,175 @@
+countries: {
+    defaults: {
+        title: 'my title'
+    },
+    europe: {
+      western: {
+        header: 
+          {
+            title: 'Западная Европа',
+            text: 'Регулярные перевозки грузов'
+          },
+        list: [
+          {
+            name: 'Австрия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Бельгия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Великобритания',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Германия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Люксембург',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Нидерланды',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Франция',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Швейцария',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            flagImgSource: 'http://placehold.it/90x90'
+          }
+        ]
+      },
+      eastern: {
+        header: 
+          {
+            title: 'Восточная Европа',
+            text: 'Регулярные перевозки грузов'
+          },
+        list: [
+          {
+            name: 'Болгария',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Венгрия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Молдавия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Польша',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Румыния',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Словакия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Украина',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Чехия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            flagImgSource: 'http://placehold.it/90x90'
+          }
+        ]
+      },
+      northern: {
+        header: 
+          {
+            title: 'Северная Европа',
+            text: 'Регулярные перевозки грузов'
+          },
+        list: [
+          {
+            name: 'Дания',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Латвия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Литва',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Финляндия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Швеция',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Эстония',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            flagImgSource: 'http://placehold.it/90x90'
+          }
+        ]
+      },
+      southern: {
+        header: 
+          {
+            title: 'Южная Европа',
+            text: 'Регулярные перевозки грузов'
+          },
+        list: [
+          {
+            name: 'Греция',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Испания',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Италия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Португалия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Сербия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Словения',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Хорватия',
+            flagImgSource: 'http://placehold.it/90x60'
+          },
+          {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            flagImgSource: 'http://placehold.it/90x90'
+          }
+        ]
+      }
+    }//europe
+  }//countries
