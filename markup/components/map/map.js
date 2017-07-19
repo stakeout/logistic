9aea6a7a16cf4cee51e825eb7580bf08a8dca8d2
Vadim Novash <novash@tut.by>
2017-07-19 17:47:04 +0300

    yandex map UX changes? add custom pin. Aviatransportation page done

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -6,13 +6,18 @@ export const map = () => {
     function init() {
         myMap = new ymaps.Map('map', {
             center: [53.88985341, 27.53529831],
-            zoom: 16
+            zoom: 16,
+            controls: ['zoomControl', 'typeSelector', 'routeEditor']
         });
-
         myPlacemark = new ymaps.Placemark([53.88985341, 27.53529831], {
-            hintContent: 'ТВ Логистик'
+            hintContent: 'ТВ Логистик',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: '/static/img/minified-svg/blue_map_marker.svg',
+            iconImageSize: [50, 42],
+            iconImageOffset: [-28, -20]
         });
-
         myMap.geoObjects.add(myPlacemark);
+        myMap.behaviors.disable('scrollZoom');
     }
 };
