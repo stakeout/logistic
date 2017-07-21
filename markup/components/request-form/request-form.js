4d8b09535304da5b58719b4f9e63b1d64dbf2926
Vadim Novash <novash@tut.by>
2017-07-21 16:44:18 +0300

    trying oop clientside validation

--- markup/components/request-form/request-form.js
+++ markup/components/request-form/request-form.js
@@ -13,132 +13,100 @@ export const validation = () => {
             return this.invalidities.join('. \n');
         },
 
-        checkValidity: function (input) {
-
-            for ( let i = 0; i < this.validityChecks.length; i++ ) {
-
-                let isInvalid = this.validityChecks[i].isInvalid(input);
-                if (isInvalid) {
-                    this.addInvalidity(this.validityChecks[i].invalidityMessage);
-                }
-
-                let requirementElement = this.validityChecks[i].element;
-                if (requirementElement) {
-                    if (isInvalid) {
-                        requirementElement.classList.add('requirements__item--invalid');
-                        requirementElement.classList.remove('requirements__item--valid');
-                    } else {
-                        requirementElement.classList.remove('requirements__item--invalid');
-                        requirementElement.classList.add('requirements__item--valid');
-                    }
-                }
-
-            }
-
-        }
     };
 
-    let usernameValidityChecks = [
+    let nameValidityChecks = [
         {
             isInvalid: function (input) {
                 return input.value.length < 3;
             },
-            invalidityMessage: 'This input needs to be at least 3 characters long',
-            element: document.querySelector('label[for=\'username\'] li:nth-child(1)')
+            invalidityMessage: 'Введите как минимум 3 символа'
+            // element: document.querySelector('label[for=\'username\'] li:nth-child(1)')
         },
         {
             isInvalid: function (input) {
-                let illegalCharacters = input.value.match(/[^a-zA-Z0-9]/g);
+                let illegalCharacters = input.value.match(/[^а-яА-Яa-zA-Z -]/g);
                 return illegalCharacters ? true : false;
             },
-            invalidityMessage: 'Only letters and numbers are allowed',
-            element: document.querySelector('label[for=\'username\'] li:nth-child(2)')
+            invalidityMessage: 'Пишите только буквы, пробел и дефис'
+            // element: document.querySelector('label[for=\'username\'] li:nth-child(2)')
         }
     ];
 
-    let passwordValidityChecks = [
+    let companyValidityChecks = [
         {
             isInvalid: function (input) {
-                return input.value.length < 8 || input.value.length > 100;
+                return input.value.length < 3;
             },
-            invalidityMessage: 'This input needs to be between 8 and 100 characters',
-            element: document.querySelector('label[for=\'password\'] li:nth-child(1)')
+            invalidityMessage: 'Введите как минимум 3 символа'
+            // element: document.querySelector('label[for=\'username\'] li:nth-child(1)')
         },
         {
             isInvalid: function (input) {
-                return !input.value.match(/[0-9]/g);
+                let illegalCharacters = input.value.match(/[^а-яА-Яa-zA-Z -]/g);
+                return illegalCharacters ? true : false;
             },
-            invalidityMessage: 'At least one number are required',
-            element: document.querySelector('label[for=\'password\'] li:nth-child(2)')
-        },
-        {
-            isInvalid: function (input) {
-                return !input.value.match(/[a-z]/g);
-            },
-            invalidityMessage: 'At least one lowercase letter is required',
-            element: document.querySelector('label[for=\'password\'] li:nth-child(3)')
-        },
-        {
-            isInvalid: function (input) {
-                return !input.value.match(/[A-Z]/g);
-            },
-            invalidityMessage: 'At least one uppercase letter is required',
-            element: document.querySelector('label[for=\'password\'] li:nth-child(4)')
-        },
-        {
-            isInvalid: function (input) {
-                return !input.value.match(/[\!\@\#\$\%\^\&\*]/g);
-            },
-            invalidityMessage: 'You need one of the required special characters',
-            element: document.querySelector('label[for=\'password\'] li:nth-child(5)')
+            invalidityMessage: 'Пишите только буквы, пробел и дефис'
+            // element: document.querySelector('label[for=\'username\'] li:nth-child(2)')
         }
     ];
 
-    let passwordRepeatValidityChecks = [
+    let phoneValidityChecks = [
         {
             isInvalid: function (input) {
-                return passwordRepeatInput.value != passwordInput.value;
+                return input.value.length < 3; // что это за проверка такая странная как пароль))
             },
             invalidityMessage: 'This password needs to match the first one'
         }
     ];
 
-    function checkInput(input) {
-        input.CustomValidation.invalidities = [];
-        input.CustomValidation.checkValidity(input);
+// почему бы тут не вешать все классы
+    function checkInput(inputValidation) {
+        inputValidation.CustomValidation.invalidities = [];
+        let input = inputValidation.input;
 
-        if ( input.CustomValidation.invalidities.length === 0 && input.value !== '' ) {
-            input.setCustomValidity('');
-        } else {
-            let message = input.CustomValidation.getInvalidities();
-            input.setCustomValidity(message);
+        for (let i = 0; i < inputValidation.CustomValidation.validityChecks.length; i++) {
+            let isInvalid = inputValidation.CustomValidation.validityChecks[i].isInvalid(input);
+            if (isInvalid) {
+                input.classList.add('error');
+            } else {
+                input.classList.add('success');
+                input.classList.remove('error');
+
+            }
         }
+
     }
 
-    let usernameInput = document.querySelector('#username');
-    var passwordInput = document.querySelector('#password');
-    var passwordRepeatInput = document.querySelector('#password_repeat');
+    let usernameInput = {};
+    usernameInput.input = document.querySelector('#name');
+    let companyInput = {};
+    companyInput.input = document.querySelector('#company');
+    let phoneInput = {};
+    phoneInput.input = document.querySelector('#phone');
 
     usernameInput.CustomValidation = new CustomValidation();
-    usernameInput.CustomValidation.validityChecks = usernameValidityChecks;
+    usernameInput.CustomValidation.validityChecks = nameValidityChecks;
 
-    passwordInput.CustomValidation = new CustomValidation();
-    passwordInput.CustomValidation.validityChecks = passwordValidityChecks;
+    companyInput.CustomValidation = new CustomValidation();
+    companyInput.CustomValidation.validityChecks = companyValidityChecks;
 
-    passwordRepeatInput.CustomValidation = new CustomValidation();
-    passwordRepeatInput.CustomValidation.validityChecks = passwordRepeatValidityChecks;
+    phoneInput.CustomValidation = new CustomValidation();
+    phoneInput.CustomValidation.validityChecks = phoneValidityChecks;
 
-    let inputs = document.querySelectorAll('input:not([type=\'submit\'])');
-    for ( let i = 0; i < inputs.length; i++ ) {
-        inputs[i].addEventListener('keyup', function () {
-            checkInput(this);
-        });
-    }
+    const inputValidation = [usernameInput, companyInput, phoneInput];
+
+    let inputs = document.querySelectorAll('input:not([type=\'submit\']):not([type=\'checkbox\']):required');
+    // for ( let i = 0; i < inputs.length; i++ ) {
+    //     inputs[i].addEventListener('keyup', function () {
+    //         checkInput(this);
+    //     });
+    // }
 
     let submit = document.querySelector('input[type=\'submit\']');
     submit.addEventListener('click', function () {
-        for ( let i = 0; i < inputs.length; i++ ) {
-            checkInput(inputs[i]);
+        for ( let i = 0; i < inputValidation.length; i++ ) {
+            checkInput(inputValidation[i]);
         }
     });
 };
