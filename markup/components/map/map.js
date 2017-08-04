cfd301c4dc7264893d62daf517df0ed099b4162c
Vadim Novash <novash@tut.by>
2017-08-04 15:18:20 +0300

    ya.map controls changeed, mobile menu bootm padding, map gradient

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -7,7 +7,7 @@ export const map = () => {
         myMap = new ymaps.Map('map', {
             center: [53.88985341, 27.53529831],
             zoom: 16,
-            controls: ['zoomControl']
+            controls: []
         });
         myPlacemark = new ymaps.Placemark([53.88985341, 27.53529831], {
             hintContent: 'ТВ Логистик',
@@ -16,6 +16,13 @@ export const map = () => {
             iconImageHref: 'static/img/minified-svg/blue_map_marker.svg',
             iconImageSize: [50, 42],
             iconImageOffset: [-28, -20]
+        });
+        myMap.controls.add('zoomControl', {
+            size: 'small',
+            position: {
+                bottom: 50,
+                right: 10
+            }
         });
         myMap.geoObjects.add(myPlacemark);
         myMap.behaviors.disable('scrollZoom');
