34fc681bd8416946515aa106f7e51c8c050a1ff6
Vadim Novash <novash@tut.by>
2017-08-30 19:22:07 +0300

    js file optimized. clear console

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -1,31 +1,34 @@
 export const map = () => {
-    ymaps.ready(init);
-    let myMap,
-        myPlacemark;
+    const map = document.querySelector('#map');
+    if (map) {
+        ymaps.ready(init);
+        let myMap,
+            myPlacemark;
 
-    function init() {
-        myMap = new ymaps.Map('map', {
-            center: [53.88985341, 27.53529831],
-            zoom: 16,
-            controls: []
-        });
-        myPlacemark = new ymaps.Placemark([53.88985341, 27.53529831], {
-            hintContent: 'ТВ Логистик'
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'static/img/minified-svg/blue_map_marker.svg',
-            iconImageSize: [50, 42],
-            iconImageOffset: [-28, -20]
-        });
-        myMap.controls.add('zoomControl', {
-            size: 'small',
-            position: {
-                bottom: 50,
-                right: 10
-            }
+        function init() {
+            myMap = new ymaps.Map('map', {
+                center: [53.88985341, 27.53529831],
+                zoom: 16,
+                controls: []
+            });
+            myPlacemark = new ymaps.Placemark([53.88985341, 27.53529831], {
+                hintContent: 'ТВ Логистик'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'static/img/minified-svg/blue_map_marker.svg',
+                iconImageSize: [50, 42],
+                iconImageOffset: [-28, -20]
+            });
+            myMap.controls.add('zoomControl', {
+                size: 'small',
+                position: {
+                    bottom: 50,
+                    right: 10
+                }
 
-        });
-        myMap.geoObjects.add(myPlacemark);
-        myMap.behaviors.disable('scrollZoom');
+            });
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom');
+        }
     }
 };
--- markup/components/vacancies/vacancies.js
+++ markup/components/vacancies/vacancies.js
@@ -1,9 +1,9 @@
 export const modalVacancies = () => {
-    const modalInfo = document.querySelector('#modal-info');
-    const modalContent = modalInfo.querySelector('.wrapper');
     const vacancyItem = document.querySelectorAll('.vacancies__item');
     const overlay = document.querySelector('.overlay');
     const vacancyBtn = document.querySelector('.vacancies-btn');
+    const modalContent = document.querySelector('.wrapper');
+    const modalInfo = document.querySelector('#modal-info');
     const popupForm = document.querySelector('.vacancies__modal');
 
     function modal(e) {
@@ -13,17 +13,21 @@ export const modalVacancies = () => {
         overlay.classList.toggle('hide');
         modalContent.innerHTML = data;
     }
-    function popup(e) {
-        e.preventDefault();
-        modalInfo.classList.toggle('hide');
-        popupForm.classList.toggle('hide');
+    if (vacancyItem) {
+        overlay.addEventListener('click', function () {
+            modalInfo.classList.add('hide');
+            popupForm.classList.add('hide');
+        });
+        for (let i = 0; i < vacancyItem.length; i++) {
+            vacancyItem[i].addEventListener('click', modal);
+        }
     }
-    overlay.addEventListener('click', function () {
-        modalInfo.classList.add('hide');
-        popupForm.classList.add('hide');
-    });
-    vacancyBtn.addEventListener('click', popup);
-    for (let i = 0; i < vacancyItem.length; i++) {
-        vacancyItem[i].addEventListener('click', modal);
+    if (vacancyBtn) {
+        vacancyBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            modalInfo.classList.toggle('hide');
+            popupForm.classList.toggle('hide');
+        });
     }
+
 };
